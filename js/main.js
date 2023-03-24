a9905403d31e48e9b064ef539f64b5759c0942ff
Obed Amoasi <obedamoasi@gmail.com>
2023-03-24 14:10:44 +0000

    Update main.js
    update coming soon date

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2023/02/07';
+        var finalDate = '2023/07/11';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
